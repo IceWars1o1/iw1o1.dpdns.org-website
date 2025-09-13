2e04dbfb085b80664a3b08d66b57f512353db5c0
DanMoAB <abqyc@outlook.com>
2025-09-13 10:07:52 +0800

    Fixed Known Issues

--- infmc/backup/old/scripts/shared.js
+++
@@ -1,7 +0,0 @@
-function redirectToCopyright(){
-    window.location.href="https://www.iw1o1.dpdns.org/copyright";
-}
-
-function newTab(){
-    window.location.href="https://infmc.dpdns.org/new";
-}
